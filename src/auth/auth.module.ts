a344a8544138bd19f554006999841dae2790c312
ericafenyo <ericafenyo@yahoo.com>
2022-03-21 22:30:16 +0100

    feat: Return a valid refresh token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { LocalStrategy } from './local.strategy';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RefreshToken, RefreshTokenSchema } from './refresh-token.schema';
 
 const secret = process.env.JWT_SECRET;
 @Module({
@@ -13,6 +15,9 @@ const secret = process.env.JWT_SECRET;
       defaultStrategy: 'jwt',
     }),
     JwtModule.register({ secret }),
+    MongooseModule.forFeature([
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
     UserModule,
   ],
   providers: [AuthService, AuthResolver, LocalStrategy],
