6072de2d3f7f1d000a58bb2c531cf6ed3698edb5
Eric Afenyo <ericafenyo@yahoo.com>
2022-07-14 22:37:26 +0200

    Revert "refactor: Adapt adventure saving to new schema"
    This reverts commit b1bf8bec1e8bbfcaee7cd8fe98fa51283cc54437.

--- src/adventure/adventure.resolver.ts
+++ src/adventure/adventure.resolver.ts
@@ -2,24 +2,24 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { AdventureService } from './adventure.service';
 import { AuthenticatedUser, CurrentUser } from '../auth/auth.decorator';
 import { Adventure, AdventureInput } from './adventure.types';
-import { UseGuards } from '@nestjs/common';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/auth.guard';
 
 @Resolver()
 export class AdventureResolver {
-  constructor(private service: AdventureService) { }
+  constructor(private adventureService: AdventureService) {}
 
   @Query(() => [Adventure])
   async adventures(): Promise<Adventure[]> {
-    return await this.service.find();
+    return this.adventureService.find();
   }
 
-  @Mutation(() => [Adventure])
+  @Mutation(() => Adventure)
   @UseGuards(JwtAuthGuard)
-  async addAdventures(
+  async addAdventure(
     @CurrentUser() user: AuthenticatedUser,
-    @Args({ name: 'params', type: () => [AdventureInput] }) inputs: AdventureInput[],
-  ): Promise<Adventure[]> {
-    return await this.service.addAdventures(user.id, inputs);
+    @Args('param') adventureInput: AdventureInput,
+  ): Promise<Adventure> {
+    return await this.adventureService.saveAdventure(user.id, adventureInput);
   }
 }
