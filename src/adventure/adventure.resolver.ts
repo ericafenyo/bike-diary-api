b1bf8bec1e8bbfcaee7cd8fe98fa51283cc54437
ericafenyo <ericafenyo@yahoo.com>
2022-07-14 22:36:22 +0200

    refactor: Adapt adventure saving to new schema

--- src/adventure/adventure.resolver.ts
+++ src/adventure/adventure.resolver.ts
@@ -2,24 +2,24 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { AdventureService } from './adventure.service';
 import { AuthenticatedUser, CurrentUser } from '../auth/auth.decorator';
 import { Adventure, AdventureInput } from './adventure.types';
-import { ForbiddenException, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/auth.guard';
 
 @Resolver()
 export class AdventureResolver {
-  constructor(private adventureService: AdventureService) {}
+  constructor(private service: AdventureService) { }
 
   @Query(() => [Adventure])
   async adventures(): Promise<Adventure[]> {
-    return this.adventureService.find();
+    return await this.service.find();
   }
 
-  @Mutation(() => Adventure)
+  @Mutation(() => [Adventure])
   @UseGuards(JwtAuthGuard)
-  async addAdventure(
+  async addAdventures(
     @CurrentUser() user: AuthenticatedUser,
-    @Args('param') adventureInput: AdventureInput,
-  ): Promise<Adventure> {
-    return await this.adventureService.saveAdventure(user.id, adventureInput);
+    @Args({ name: 'params', type: () => [AdventureInput] }) inputs: AdventureInput[],
+  ): Promise<Adventure[]> {
+    return await this.service.addAdventures(user.id, inputs);
   }
 }
