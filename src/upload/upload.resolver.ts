c9d5184da45be8948994a72742c796370dae3822
ericafenyo <ericafenyo@yahoo.com>
2022-07-21 01:36:30 +0200

    feat: Add default file storage server

--- src/upload/upload.resolver.ts
+++ src/upload/upload.resolver.ts
@@ -1,14 +1,19 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
 import { FileUpload } from 'graphql-upload';
+import { UploadService } from './upload.service';
+import { UploadedFile } from './upload.types';
+import { ReadStream } from 'fs';
 
 @Resolver()
 export class UploadResolver {
+  constructor(private uploadService: UploadService) {}
 
-  @Mutation(() => Boolean)
-  async uploadFile(@Args('file', { type: () => GraphQLUpload }) file: FileUpload): Promise<boolean> {
-    console.log(file);
-
-    return true;
+  @Mutation(() => UploadedFile)
+  async uploadFile(
+    @Args('file', { type: () => GraphQLUpload }) file: FileUpload,
+  ): Promise<UploadedFile> {
+    const stream: ReadStream = file.createReadStream();
+    return await this.uploadService.upload(stream);
   }
 }
